6abddf3ebb641c3738c6783bc6c4574491ce9f1d
Luis David Work <davidluciovio@gmail.com>
2025-10-22 18:28:37 -0600

    feat(routing): configura las rutas principales de Home y Login
    Se establece la estructura de enrutamiento base de la aplicación.
    
    - Se añaden las rutas principales para '/login' y la ruta raíz ('/') que carga la página 'Home'.
    - Se incluye una ruta comodín ('**') que redirige a 'Home'.
    - Se crean los componentes 'standalone' 'Home' y 'Login' como marcadores de posición para estas nuevas rutas.
    
    refactor(ui):
    - Se ajusta el layout de la 'nav-bar' a un formato de fila ('flex-row') para la futura distribución de su contenido.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,16 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    
+    {
+        path: 'login',
+        loadComponent: () => import('./modules/auth/pages/login/login').then(m => m.Login)
+    },
+    {
+        path: '',
+        loadComponent: () => import('./modules/home/pages/home/home').then(m => m.Home)
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
 ];
